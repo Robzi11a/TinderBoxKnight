0292d1e8308ddb8b8da0ef73823f2be7d687de96
RobZi11a <r3avagg@gmail.com>
2021-11-18 17:22:27 +0000

    added first level .txt file
    repository structure

--- settings.py
+++ settings.py
@@ -7,3 +7,10 @@ class Settings:
         self.screen_width = 1240
         self.screen_height = 620
         self.bg_colour = (3.5, 3.9, 7.8)
+        self.black_colour = (0, 0, 0)
+
+        block_height = 100
+        block_width = 100
+
+        self.level1width = 7
+        self.level1length = 10
