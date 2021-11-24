aa8409c61d0777a7f8bb8c0d21f3448fdef7faa2
RobZi11a <r3avagg@gmail.com>
2021-11-24 12:13:58 +0000

    removed scan torch feature
    gameplay feature

--- scan.py
+++ scan.py
@@ -29,8 +29,6 @@ class Scanner:
                     print("yes, ", xIndex, " < ", TILES_VERTICAL, " ", yIndex, " < ", TILES_HORIZONTAL)
                     if self.original_array[xIndex][yIndex] == 'hs':
                         self.count += 1
-                    if self.original_array[xIndex][yIndex] == 'ht':
-                        self.count += 50
                     if self.tiles[xIndex][yIndex] == 'ls':
                         continue
                     # self.tiles[xIndex][yIndex] = 'st'
@@ -44,8 +42,6 @@ class Scanner:
         label = ""
         if self.count > 1:
             label = "There are " + str(self.count) + " enemies around."
-        if self.count > 50:
-            label = "You found the torch, well done!"
         else:
             label = "There is " + str(self.count) + " enemy around."
 
